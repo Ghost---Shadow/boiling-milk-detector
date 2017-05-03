194868a5ba0407b5204e9ce00cf604c4456705e5
Souradeep Nanda <souradeep.nanda2014@vit.ac.in>
2017-05-03 13:38:28 +0530

    DeepDreamForBasic

---
+++ DeepDreamTest/Basic/BasicTest.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import numpy as np
+import cv2
+
+modelDirectory = "../models/BasicModel/"
+scriptpath = "../models/BasicModel/"
+sys.path.append(os.path.abspath(scriptpath))
+
+from BasicModel import *
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+saver.restore(sess,modelDirectory)
+
+def dream(layer = -1,ITERATIONS = 50):
+  img_noise = np.random.uniform(size=(videoHeight,videoWidth))
+  #img_noise = np.ones((videoHeight,videoWidth)) * .5
+  total_image = None
+
+  for channel in range(h[layer].get_shape().as_list()[-1]):
+    try:
+      t_obj = h[layer][:,:,:,channel]
+    except:
+      t_obj = h[layer][:,channel]
+    t_score = tf.reduce_mean(t_obj)
+    t_grad = tf.gradients(t_score,X)[0]
+    img = img_noise.copy()
+    img = img.reshape((1,videoHeight,videoWidth,1))
+
+    for i in range(ITERATIONS):
+      g,score = sess.run([t_grad,t_score],{X:img,p_keep_conv: 1,p_keep_hidden: 1})
+      g /= g.std()+1e-8
+      step = 1
+      img += g*step
+    print(channel,score)
+
+    img = (img-img.mean())/max(img.std(), 1e-4)*.1 + 0.5     
+    if total_image is None:
+      total_image = img.reshape((videoHeight,videoWidth))
+    else:
+      total_image = np.hstack((total_image,img.reshape((videoHeight,videoWidth))))
+  cv2.imwrite('Total_%s.png'%layer,total_image * 255)
+
+def dreamAll(ITERATIONS = 50):
+  for i in range(len(h)):
+    dream(i,ITERATIONS)
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Boiling Milk Detector
 A WIP Machine Learning app using Tensorflow which will be able to tell if a pot of milk is boiling.
-Used the CNN architecture of [nlintz](https://github.com/nlintz/TensorFlow-Tutorials/blob/master/05_convolutional_net.py).
 
 ## Requirements
 
@@ -12,5 +11,5 @@ Used the CNN architecture of [nlintz](https://github.com/nlintz/TensorFlow-Tutor
 ## How to use
 
 1. In the `preprocess` folder, run `ProcessVideos.py` > `WindowsToOneHot.py` > `Combine.py` in order.
-2. Run `BasicCNN.py`
+2. Run `BasicCNN.py` or `TemporalCNN.py` or `TinyCNN.py`
 3. After the model is trained, select the video to test in `ManualVerification.py` and run
